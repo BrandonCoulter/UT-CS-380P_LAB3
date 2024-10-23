986bae3443ee766f7d890d44f5e4f2d251345ea1
Brandon Coulter <bmcnmt@aol.com>
2024-10-22 23:17:00 -0500

    Revereted to fix seq (FIXED CASE 3/5 ALSO) and added updated checker.py

--- src/sequential/sequential.go
+++ src/sequential/sequential.go
@@ -15,10 +15,7 @@ func BSTSeqential(data []string, args *utilities.ArgumentParser){
 	// Store the start time
 	timer := utilities.Timer{Start: time.Now()}
 
-    // Create a list of binary Search trees
-    bst_trees := make([]*utilities.BSTRootNode, 0)
-
-    // Map of hash numbers to slice of root nodes
+	// Map of hash numbers to slice of root nodes
 	hashes := make(map[int][]*utilities.BSTRootNode)
 
 	// Iterate lines in the input file
@@ -36,25 +33,25 @@ func BSTSeqential(data []string, args *utilities.ArgumentParser){
 
 		}
 
-		// Build the BST and calculate Hash #
+		// Build the BST and calculate Hash # and assign to Hash map
 		if len(bst) > 0 {
 			root := BuildBST(bst, ID) // Returns the root of the BST
-			root.GenHashNumber(root.Root, args, false) // Generate the Hash Value without In place order
+			root.GenHashNumber(root.Root, args, true) // Generate the Hash Value and In place order
+			
+			 // Print a new line because GenHashNumbers prints out in place order
+			if *args.IsPrint{fmt.Printf("\n")}
 
-            bst_trees = append(bst_trees, root) // Appended the root node to the list of BSTs
+			// Append the bst to a Hash group or create a new group if non-existent
+			hashes[root.Hash] = append(hashes[root.Hash], root)
 		}
 	}
 
 	// Print sequential Hashtime
 	fmt.Printf("hashTime: %f\n", timer.TrackTime().Seconds())
 	
-	// Print find hash groups and preint them
+	// Print Hash groups
 	if *args.Data_workers > 0 {
-        timer.Start = time.Now() // Restart timer for Hash BST calculation/grouping
-        hashes = GroupBST(bst_trees, args)
-
-        utilities.PrintHashGroups(timer.TrackTime().Seconds(), hashes) // Print the hash groups
-        
+		utilities.PrintHashGroups(timer.TrackTime().Seconds(), hashes)
 	}
 	
 	// Print Comp groups
@@ -83,24 +80,6 @@ func BuildBST(data []int, id int) *utilities.BSTRootNode {
 
 	// Return a reference to the root node
 	return &root
-}
-
-func GroupBST(bst_trees []*utilities.BSTRootNode, args *utilities.ArgumentParser) map[int][]*utilities.BSTRootNode {
-
-    // Map of hash numbers to slice of root nodes
-	hashes := make(map[int][]*utilities.BSTRootNode)
-    
-    for _, root := range(bst_trees) {
-        
-        root.GenHashNumber(root.Root, args, true) // Regenerate the Hash value with In place order 
-        // Print a new line because GenHashNumbers prints out in place order
-        if *args.IsPrint{fmt.Printf("\n")}
-
-        // Append the bst to a Hash group or create a new group if non-existent
-        hashes[root.Hash] = append(hashes[root.Hash], root)
-    }
-
-    return hashes
 }
 
 // Function to compare BST based on in-order traversal hash
